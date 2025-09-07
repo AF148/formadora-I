f082a61082ade6a458ff922a3c423cef6c1f1822
AF148 <123138458+AF148@users.noreply.github.com>
2025-09-07 20:02:50 -0300

    Add files via upload

---
+++ src/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonAvatar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+      IonHeader, 
+      IonToolbar, 
+      IonTitle, 
+      IonContent,
+      IonCard, 
+      IonCardHeader, 
+      IonCardTitle, 
+      IonCardSubtitle, 
+      IonButton, 
+      IonAvatar
+    ],
+})
+export class HomePage {
+
+  title:string = 'Cartão de Identificação';
+  name:string = 'André Filipe Ildefonso dos Santos';
+  matricula:string = 'COD: 2025-2-PAS23103-CG-ADS0301N';
+  constructor(private router: Router) {}
+
+
+  handleClick() {
+      this.router?.navigateByUrl('/about');
+  }
+
+  
+}
